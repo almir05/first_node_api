50cb4cf086d9f7191155072556bd136ad944b90e
almir05 <almir.marcelo08@gmail.com>
2021-09-16 21:50:43 -0400

    Criando a primeira rota da API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const http = require('http')
 
+const users = require('./mocks/users')
+
 const server = http.createServer((req,res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.end('<h1>Hello world!</h1>')
+  console.log(`Request method: ${req.method} | Endpoint: ${req.url}`)
+
+  if(req.url === '/users' && req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(users))
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'})
+    res.end(`Cannot ${req.method} ${req.url}`)
+  }
+
 })
 
 server.listen(3000, () => console.log('Server started at http://localhost:3000'))
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Almir'
+  },
+  {
+    id: 2,
+    name: 'Marcelo'
+  },
+  {
+    id: 3,
+    name: 'Segundo'
+  },
+]
